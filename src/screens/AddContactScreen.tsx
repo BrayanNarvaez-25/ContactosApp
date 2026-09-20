d82767b696149f6d9eeb4f1c8913274a93751f40
BrayanNarvaez-25 <brayann251107g@gmail.com>
2026-09-20 18:55:58 -0500

    Implement navigation and screens for contact management

---
+++ src/screens/AddContactScreen.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native'
+
+export default function AddContactScreen(){
+    return (
+        <View>
+            <Text>Formulario de contacto</Text>
+        </View>
+    )
+}
---
+++ src/screens/ContactListScreen.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native'
+
+export default function ContactScreen(){
+    return (
+        <View>
+            <Text>Pantalla de Contactos</Text>
+        </View>
+    )
+}
